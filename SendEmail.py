016a84e2a74cb9d6fa902f3546e8a1d912dcc4db
Oscar Palafox Verna <oscarpalafoxverna@gmail.com>
2019-12-25 18:59:49 +0100

    Working with utf-8 encoding and hidden credentials

--- SendEmail.py
+++ SendEmail.py
@@ -6,8 +6,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 
 # Credentials of the email account
-gmail_user = "regalosfamiliapalafox@gmail.com"
-gmail_password = "Gascons1"
+gmail_user = open('user.txt', 'r', encoding='utf-8').read()
+gmail_password = open('credentials.txt', 'r', encoding='utf-8').read()
 
 # Log in the gmail server
 server = SMTP_SSL('smtp.gmail.com', 465)
@@ -15,19 +15,19 @@ server.ehlo()
 server.login(gmail_user, gmail_password)
 
 # Fetch current date
-current = datetime.now()
+current_date = datetime.now()
 
 
 def send_email(receiver, gifted):
     email = MIMEMultipart()
     email['From'] = 'Regalos Navidad'
     email['To'] = receiver
-    email['Subject'] = f"Regalo invisible para el {current.year + 1}"
+    email['Subject'] = f"Regalo invisible para el {current_date.year + 1}"
 
     # The body_email.txt can be changed to anything, just be sure to include the variable {0} in the body
-    body = open("body_email.txt", "r").read().format(gifted)
+    body = open('body_email.txt', 'r', encoding='utf-8').read().format(gifted)
 
-    email.attach(MIMEText(body, 'plain'))
+    email.attach(MIMEText(body))
 
     email_text = email.as_string()
 
